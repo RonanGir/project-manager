34354b06ce9d111838ba7c03c65858db7f917e11
Ronan Girault <girault.ronan@gmail.com>
2018-09-26 17:25:46 +0200

    Add associations to Project + add data to seed.rb

--- app/models/project.rb
+++ app/models/project.rb
@@ -1,5 +1,6 @@
 class Project < ApplicationRecord
   has_many :workers
+  has_many :tasks
 
   validates :name, :description, :tag, presence: true
 end
--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,111 @@
 #
 #   movies = Movie.create([{ name: 'Star Wars' }, { name: 'Lord of the Rings' }])
 #   Character.create(name: 'Luke', movie: movies.first)
+puts 'Cleaning database...'
+Project.destroy_all
+
+puts 'Creating users...'
+users_attributes = [
+  {
+    # user_id: ,
+    # tasks_id: ,
+    firstname:     'John',
+    lastname:      'Doe',
+    email:         'john@gmail.com',
+    photo:         ,
+    cost_per_hour: 90,
+    role:          'developper'
+  },
+  {
+    # user_id: ,
+    # tasks_id: ,
+    firstname:     'Chuck',
+    lastname:      'Norris',
+    email:         'chucky@gmail.com',
+    photo:         ,
+    cost_per_hour: 140,
+    role:          'CEO'
+  },
+  {
+    # user_id: ,
+    # tasks_id: ,
+    firstname:     'Jane',
+    lastname:      'Done',
+    email:         'jane@gmail.com',
+    photo:         ,
+    cost_per_hour: 140,
+    role:          'manager'
+  },
+  {
+    # user_id: ,
+    # tasks_id: ,
+    firstname:     'Joan',
+    lastname:      'Watson',
+    email:         'joan@gmail.com',
+    photo:         ,
+    cost_per_hour: 60,
+    role:          'apprenticeship'
+  }
+]
+
+
+Project.create!(users_attributes)
+puts 'Finished!'
+
+puts 'Creating projects...'
+projects_attributes = [
+  {
+    # user_id: ,
+    # tasks_id: ,
+    name:         'LLA Avocat',
+    description:  'These guys are lawyer and want a new site to find more clients',
+    tag:          'Refonte de site',
+    total_cost:   900,
+    sold_price:   900,
+    sold_hours:   7,
+    total_time:   7,
+    start_date: ,
+    end_date:
+  },
+  {
+    # user_id: ,
+    # tasks_id: ,
+    name:         'Bonjour Concept Store',
+    description:  'A cool shop in Paris',
+    tag:          'Refonte de site',
+    total_cost:   1000,
+    sold_price:   900,
+    sold_hours:   7,
+    total_time:   8,
+    start_date: ,
+    end_date:
+  },
+  {
+    # user_id: ,
+    # tasks_id: ,
+    name:         'Jet Evasion',
+    description:  'Many cool activities to do on the sea in La Baule',
+    tag:          'Refonte de site',
+    total_cost:   800,
+    sold_price:   900,
+    sold_hours:   7,
+    total_time:   6,
+    start_date: ,
+    end_date:
+  },
+  {
+    user_id: ,
+    tasks_id: ,
+    name:         'AS24',
+    description:  'Biggest international Adwords campaign',
+    tag:          'SEA',
+    total_cost:   1000,
+    sold_price:   900,
+    sold_hours:   7,
+    total_time:   8,
+    start_date: ,
+    end_date:
+  }
+]
+Project.create!(projects_attributes)
+puts 'Finished!'
